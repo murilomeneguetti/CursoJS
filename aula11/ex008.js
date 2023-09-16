47c798283d175909e1e80050f1abbe0638a4bc9b
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-09-16 08:48:05 -0300

    if

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 80.5;
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade máxima permitida. Multado!`);
+} else {
+    console.log(`Dirija usando o cinto de segurança`);
+}

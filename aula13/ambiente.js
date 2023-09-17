cedab08308af90c8bd2dda3dc28464c8d7fd1f74
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-09-17 09:22:52 -0300

    for

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,23 @@
+
 var c = 1
+console.log('while');
 while (c <= 6) {
     console.log(c);
     c++;
 }
 
+console.log('----------------');
+
 var i = 1
+console.log('do')
 do {
     console.log(i)
     i++;
 } while(i <= 6)
+
+console.log('----------------');
+
+console.log('for')
+for (var i = 1; i <= 6; i++) {
+    console.log(i)
+}

f15a8d56b5b5fa88f923526738f447a3d7e71aa5
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-09-17 08:56:08 -0300

    repeticoes

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while (c <= 6) {
+    console.log(c);
+    c++;
+}
+
+var i = 1
+do {
+    console.log(i)
+    i++;
+} while(i <= 6)
---
+++ aula13/node_bd6f9aba56fba.tmp.js
@@ -0,0 +1,10 @@
+var c = 1
+while (c <= 6) {
+    console.log(c);
+    c++;
+}
+
+var i = 1
+do {
+    console.log(i)
+} while(i <= 6)

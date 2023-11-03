afcbd7a2e3f82a32fb6761c71826a6b6d703f84e
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-03 09:54:10 -0300

    exercicio tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('numero');
+    let tab = document.getElementById('seltab');
+
+    if (num.value.length == 0) {
+        window.alert('Obrigatório informar um número.')
+    } else {
+        let n = Number(num.value);
+        tab.innerHTML = '';
+        for(let i = 0; i <= 10; i++){
+            //res.innerHTML += `${i} x ${n} || `;
+            let item = document.createElement('option');
+            item.text = `${n} x ${i} = ${n*i}`;
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    }
+    
+
+}
